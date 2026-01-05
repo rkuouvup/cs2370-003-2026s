03e7262edadf9ac54cdd869f751c76977fdc3b43
rkuouvup <rkuouvup@gmail.com>
2026-01-05 12:39:36 -0700

    Initial Commit

---
+++ cs2370-003-2026s/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cs2370-003-2026s
+//
+//  Created by Rita Kuo on 1/5/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
