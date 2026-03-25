9ffb85cf80f17416bc2d222d85b699cde9d156de
rkuouvup <rkuouvup@gmail.com>
2026-03-25 12:51:17 -0600

    Mar 25 in-class exercise

--- cs2370-003-2026s/inheritance.cpp
+++ cs2370-003-2026s/inheritance.cpp
@@ -13,9 +13,10 @@ public:
     void SetName(string name) {this->name = name;}
     void SetAddress(string address) {this->address = address;}
     string GetName() const {return name;}
-    string GetDescription() const {
+    virtual string GetDescription() const {
         return name + " -- " + address;
     }
+    virtual string GetHours() const = 0;
 protected:
     string name;
     string address;
@@ -28,8 +29,12 @@ public:
     void DisplayRestaurant() const {
         cout << GetName() << "--" << address << "--" << rating << endl;
     }
-    string GetDescription() const {
+    string GetDescription() const override {
         return Business::GetDescription() + "\n\tRating: " + to_string(rating);
+    }
+    
+    string GetHours() const override {
+        return "8 AM to 5 PM";
     }
     
 private:
@@ -38,22 +43,41 @@ private:
 
 
 
+void DriveTo(Business* busPtr) {
+    cout << "Drive to " << busPtr->GetDescription() << endl;
+}
+
+void foo(int* a) {
+    cout << *a << endl;
+}
 
 
 int main() {
-    Business b1;
+    //int i = 10;
+    //double d = 25.8;
+    //foo(&d);
+    
+    
+    //Business b1;
     Restaurant r1;
     
+    //b1.SetName("IEEE");
+    //b1.SetAddress("New Jersey");
     
-    b1.SetName("IEEE");
-    b1.SetAddress("New Jersey");
+    //r1.SetName("Burger King");
+    //r1.SetAddress("University Parkway");
+    //r1.SetRating(3);
     
-    r1.SetName("Burger King");
-    r1.SetAddress("University Parkway");
+    //DriveTo(&r1);
+    //DriveTo(&b1);
+    
+    //b1.GetDescription();
+    //r1.GetDescription();
+    
     //r1.DisplayRestaurant();
     
-    cout << b1.GetDescription() << endl;
-    cout << r1.GetDescription() << endl;
+    //cout << b1.GetDescription() << endl;
+    //cout << r1.GetDescription() << endl;
     
     
     
