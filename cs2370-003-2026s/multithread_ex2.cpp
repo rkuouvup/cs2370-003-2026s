b26698a09349a25df70f929f3e2076877b754382
rkuouvup <rkuouvup@gmail.com>
2026-04-08 12:48:02 -0600

    Apr 8 in-class exercise

--- cs2370-003-2026s/multithread_ex2.cpp
+++ cs2370-003-2026s/multithread_ex2.cpp
@@ -5,22 +5,28 @@
 //  Created by Rita Kuo on 4/6/26.
 //
 #include <iostream>
+#include <mutex>
 #include <thread>
 using namespace std;
 
+mutex m;
+
 void f(const string &s) {
     for (int i = 0; i < 10; i++) {
+        //m.lock();
+        lock_guard<mutex> lock(m);
         for (char c:s)
             cout << c;
         cout << endl;
+        //m.unlock();
     }
 }
 
-int main() {
+/*int main() {
     thread t1(f, "It's a Dessert Toopping");
     thread t2(f, "It's a Floor Wax");
     t1.join();
     t2.join();
     
     return 0;
-}
+}*/
---
+++ cs2370-003-2026s/multithread_ex3.cpp
@@ -0,0 +1,48 @@
+//
+//  multithread_ex3.cpp
+//  cs2370-003-2026s
+//
+//  Created by Rita Kuo on 4/8/26.
+//
+#include <iostream>
+#include <mutex>
+#include <thread>
+using namespace std;
+
+mutex mtx1;
+mutex mtx2;
+
+void john() {
+    for (int i = 0; i < 3; i++) {
+        lock(mtx1, mtx2);
+        //lock_guard<mutex> lock1(mtx1);
+        lock_guard<mutex> lock1(mtx1, adopt_lock);
+        this_thread::sleep_for(chrono::milliseconds(300));
+        //lock_guard<mutex> lock2(mtx2);
+        lock_guard<mutex> lock2(mtx2, adopt_lock);
+        cout << "John has the locks" << endl;
+    }
+}
+
+void jane() {
+    for (int i = 0; i < 3; i++) {
+        lock(mtx1, mtx2);
+        //lock_guard<mutex> lock1(mtx1);
+        lock_guard<mutex> lock2(mtx2, adopt_lock);
+        this_thread::sleep_for(chrono::milliseconds(300));
+        //lock_guard<mutex> lock2(mtx2);
+        lock_guard<mutex> lock1(mtx1, adopt_lock);
+        cout << "Jane has the locks" << endl;
+    }
+}
+
+
+int main() {
+    thread t1(john);
+    thread t2(jane);
+    
+    t1.join();
+    t2.join();
+    
+    return 0;
+}
